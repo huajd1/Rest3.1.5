ee8f5699d4cda881e3c1a45907ab821f8000b4f2
hrrer <130406103+huajd1@users.noreply.github.com>
2023-08-17 23:37:31 +0300

    Update UserRestController.java

--- src/main/java/com/example/spring/security/springsecurity/controllers/UserRestController.java
+++ src/main/java/com/example/spring/security/springsecurity/controllers/UserRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@PreAuthorize("hasRole('USER')")
 public class UserRestController {
     private final UserServiceImp userServiceImp;
 
