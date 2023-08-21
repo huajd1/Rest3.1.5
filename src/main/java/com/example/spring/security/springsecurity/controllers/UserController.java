ad8b814547892fbf91eeeb27c089b0bd2dd17c71
hrrer <130406103+huajd1@users.noreply.github.com>
2023-08-21 18:57:15 +0300

    Update UserController.java

--- src/main/java/com/example/spring/security/springsecurity/controllers/UserController.java
+++ src/main/java/com/example/spring/security/springsecurity/controllers/UserController.java
@@ -1,9 +1,7 @@
 package com.example.spring.security.springsecurity.controllers;
 
-import com.example.spring.security.springsecurity.model.User;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("user")
 public class UserController {
     @GetMapping
-    public String index(Model model, @AuthenticationPrincipal User curUser) {
-        model.addAttribute("curUser", curUser);
+    public String user() {
         return "user";
     }
 }
